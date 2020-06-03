4c962f4fa0c5c1c1ffd4aafbbc9bd29cc520a6b2
politicalcompass <pcompasslive@gmail.com>
2020-06-03 20:58:29 +0200

    attempt faster cookies

--- js/js.js
+++ js/js.js
@@ -328,6 +328,24 @@ function checkCookies() {
     }
 }
 
+function checkCookieLibright() {
+    librightCookie = getCookie('libright')
+    if (librightCookie != '') {
+        if (libright != librightCookie) {
+            switchLibright()
+        }
+    }
+}
+
+function checkCookieIndicator() {
+    indicatorCookie = getCookie('indicator')
+    if (indicatorCookie != '') {
+        if (indicator != indicatorCookie) {
+            switchIndicator()
+        }
+    }
+}
+
 function loadQ() {
     checkCookies()
     currUrl = window.location.href
